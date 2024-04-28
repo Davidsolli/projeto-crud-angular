b8e5ce3313fd7ef3a2e769757d58ec98b77f9ed0
David Oliveira <davidolisoa@gmail.com>
2024-04-28 19:15:27 -0300

    toolbar e organizando as primeiras rotas

--- Crud-angular/src/app/app-routing.module.ts
+++ Crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'list'},
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then(m => m.ListModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
